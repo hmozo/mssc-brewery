1d4d4257cd0e7636cb90552fc5cf7d5dd0590037
Hugo Mozo <hmozo@outlook.com>
2020-09-09 18:37:43 +0200

    create Customer data-model controller and service

--- src/main/java/guru/springframework/msscbrewery/MsscBreweryApplication.java
+++ src/main/java/guru/springframework/msscbrewery/MsscBreweryApplication.java
@@ -2,8 +2,10 @@ package guru.springframework.msscbrewery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"guru.springframework.msscbrewery"})
 public class MsscBreweryApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/guru/springframework/msscbrewery/service/CustomerServiceImpl.java
@@ -0,0 +1,12 @@
+package guru.springframework.msscbrewery.service;
+
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+
+import java.util.UUID;
+
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public CustomerDto getCustomerById(UUID customerId) {
+        return null;
+    }
+}
